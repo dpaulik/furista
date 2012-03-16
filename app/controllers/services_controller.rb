b5b4c9c046a6b5dfe5a12393101bf5927fabda0f
mahhek <mahhek.khan@gmail.com>
2012-03-16 11:35:15 +0500

    fix

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -16,21 +16,21 @@ class ServicesController < ApplicationController
     @posts = Post.all
   end
 
-  def create
+  def create   
     request.env['omniauth.auth']['provider'] ? service_route = request.env['omniauth.auth']['provider'] : service_route = 'no service (invalid callback)'
     omniauth = request.env['omniauth.auth']
-    if omniauth and omniauth['provider']
+    if omniauth and omniauth['provider']      
       service_route = omniauth['provider']
       if service_route == 'facebook'
         omniauth['extra']['raw_info']['email'] ? email = omniauth['extra']['raw_info']['email'] : email = ''
         omniauth['extra']['raw_info']['name'] ? name = omniauth['extra']['raw_info']['name'] : name = ''
         omniauth['extra']['raw_info']['id'] ? uid = omniauth['extra']['raw_info']['id'] : uid = ''
-        omniauth['provider'] ? provider = omniauth['provider'] : provider = ''
+        omniauth['provider'] ? provider = omniauth['provider'] : provider = ''              
       else
         render :text => omniauth.to_yaml
         return
       end
-      if uid != '' and provider != ''
+      if uid != '' and provider != ''        
         if !user_signed_in?
           auth = Service.find_by_provider_and_uid(provider.to_s, uid.to_s)
           if auth
@@ -47,7 +47,7 @@ class ServicesController < ApplicationController
                 name = name[0, 39] if name.length > 39 # otherwise our user validation will hit us
                 user = User.new :email => email, :password => 'loudsquare', :user_name => name
                 user.services.build(:provider => provider, :uid => uid, :uname => name, :uemail => email)
-                user.skip_confirmation!
+                user.skip_confirmation!               
                 if user.save
 #                  user.user_name = user.user_name.gsub("-", " ")
 #                  profile = Profile.new(:user_id => user.id,:cached_slug => user.user_name)
@@ -58,7 +58,7 @@ class ServicesController < ApplicationController
                   redirect_to "/"
                 end
                 flash[:myinfo] = 'Your account on CommunityGuides has been created via ' + provider.capitalize + '. In your profile you can change your personal information and add a local password.'
-                sign_in_and_redirect(:user, user)
+                return sign_in_and_redirect(:user, user)
               end
             else
               flash[:notice] = service_route.capitalize + ' can not be used to sign-up on CommunityGuides as no valid email address has been provided. Please use another authentication provider or use local sign-up. If you already have an account, please sign-in and add ' + service_route.capitalize + ' from your profile.'
